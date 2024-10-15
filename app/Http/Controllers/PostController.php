57152bb3fd8fcf814a9c606d567c136a3e273c51
MOhamed Thabet <mohamedthabetabdelhalem_sd23@fcis.bsu.edu.eg>
2024-10-15 18:35:00 +0300

    fix error

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\post;
 use App\Models\User;
+use Illuminate\Support\Facades\Gate;
 class PostController extends Controller
 {
     // home page
